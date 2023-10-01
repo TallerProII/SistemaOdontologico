513e3add227c2c2b0abb9137697aa41d04d95f4d
GeovanyDelaCruz <76905898@continental.edu.pe>
2023-10-01 12:30:59 -0500

    Prueba 1

---
+++ Capa_Presentacion/Controladores/index.rotes.js
@@ -0,0 +1,8 @@
+import { pool } from "../App_Start/db.js";
+
+export const index = (req, res) => res.json({ message: "welcome to my api" });
+
+export const ping = async (req, res) => {
+  const [result] = await pool.query('SELECT "pong" as result');
+  res.json(result[0]);
+};
