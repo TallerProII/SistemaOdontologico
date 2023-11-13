ca8f312da6819c387db8f643f084b8cbcaed3258
Capacoila <76365621@continental.edu.pe>
2023-11-13 17:12:03 -0500

    Actualizacion CN_citas

--- Capa_Negocio/cn_citas.js
+++ Capa_Negocio/cn_citas.js
@@ -4,14 +4,24 @@ var objCapaDato = new CD_Cita();
 
 class CN_Cita {
 
-    // CREAR
-    async createCita(pacienteId, medicoId, tratamiento, fecha, hora) {
+     // CREAR CITA
+     async createCita(pacienteId, medicoId, tratamiento, fecha, hora) {
         // Validaciones
         var message = "";
 
         if (typeof pacienteId !== "number" || typeof medicoId !== "number" || typeof tratamiento !== "string" 
             || typeof fecha !== "string" || typeof hora !== "string") {
-            message = "Error en el tipo de dato ingresado";
+            if (typeof pacienteId !== "number") {
+                message = "Error en el tipo de dato ingresado, el ID del paciente debe ser un número";
+            } else if (typeof medicoId !== "number") {
+                message = "Error en el tipo de dato ingresado, el ID del médico debe ser un número";
+            } else if (typeof tratamiento !== "string") {
+                message = "Error en el tipo de dato ingresado, el tratamiento debe ser un texto";
+            } else if (typeof fecha !== "string") {
+                message = "Error en el tipo de dato ingresado, la fecha debe ser un texto";
+            } else if (typeof hora !== "string") {
+                message = "Error en el tipo de dato ingresado, la hora debe ser un texto";
+            }
         } else {
             if (!pacienteId || !medicoId || !tratamiento || !fecha || !hora) {
                 message = "Todos los campos son obligatorios";
@@ -24,34 +34,35 @@ class CN_Cita {
         return { message: message, id: 0 };
     }
 
-    //ACTUALIZAR CITA
+    // ACTUALIZAR CITA
     async updateCita(idCita, medicoId, estado, tratamiento, fecha, hora) {
-        //filtos - reglas de negocio
+        // Validaciones
         var message = "";
 
-        if(typeof medicoId !== "number" || typeof estado !== "number" ||
+        if (typeof medicoId !== "number" || typeof estado !== "number" ||
             typeof tratamiento !== "string" || typeof fecha !== "string" ||
-            typeof hora !== "string"){
-            message = "Error en el tipo de dato ingresado"
-        }
-        else{
+            typeof hora !== "string") {
+            if (typeof medicoId !== "number") {
+                message = "Error en el tipo de dato ingresado, el ID del médico debe ser un número";
+            } else if (typeof estado !== "number") {
+                message = "Error en el tipo de dato ingresado, el estado debe ser un número";
+            } else if (typeof tratamiento !== "string") {
+                message = "Error en el tipo de dato ingresado, el tratamiento debe ser un texto";
+            } else if (typeof fecha !== "string") {
+                message = "Error en el tipo de dato ingresado, la fecha debe ser un texto";
+            } else if (typeof hora !== "string") {
+                message = "Error en el tipo de dato ingresado, la hora debe ser un texto";
+            }
+        } else {
             if (!medicoId || !estado || !tratamiento || !fecha || !hora) {
                 message = "Todos los campos son obligatorios";
             }
-        }        
-        
-        if(!message){
+        }
+
+        if (!message) {
             return await objCapaDato.updateCita(idCita, medicoId, estado, tratamiento, fecha, hora);
         }
-        return { message: message, id: 0};
-    }
-    //ELIMINAR
-    async deleteCita(idCita) {
-        return await objCapaDato.deleteCita(idCita);
-    }
-    //LISTAR CITA
-    async listCita() {
-        return await objCapaDato.listCita();
+        return { message: message, id: 0 };
     }
 
 }
